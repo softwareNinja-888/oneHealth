72a1ca62dbd642f3ae8199f4ee80d8cbf7f4b1ef
softwareNinja-888 <mlandvodlamini03@gmail.com>
2025-01-27 17:01:44 +0200

    Refactor code

--- oneHealth/src/components/Footer.jsx
+++ oneHealth/src/components/Footer.jsx
@@ -5,8 +5,8 @@ export function Footer(){
             <div className="bg-primary pt-20">
                 <div id="contact" className="text-4xl font-lora font-bold text-center text-secondary mb-16">Contact Us</div>
                 <FormFooter/>
-                <div className=" py-6 text-secondary">
-                    <div className="text-sm text-center dark:text-black">&copy; 2025 by Salt and Light Ministries</div>
+                <div className=" py-6">
+                    <div className="text-sm text-center text-secondary">&copy; 2025 by Salt and Light Ministries</div>
                 </div>
             </div>
         </>
