523a25354a4d621174d8cfc1813ed616f7be2a8e
softwareNinja-888 <mlandvodlamini03@gmail.com>
2025-01-28 10:08:35 +0200

    Add responsive design for testimonial section

--- oneHealth/src/components/Testimonials.jsx
+++ oneHealth/src/components/Testimonials.jsx
@@ -7,10 +7,14 @@ export function Testimonials(){
     return (
         <>
             <div id="testimonials" className="bg-otherLight px-5 py-20  text-primary flex flex-col gap-16 ">
-                 <SectionIntro header={<Button text="Testimonials" bg="bg-secondary" color="text-primary" width="w-52" fntSize="text-xl"/>}  name="Real Stories, Real Impact" />
-                 <TestimonialsCard author='Alex Chan' text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae libero odio illo quasi eaque molestias obcaecati inventore atque ad est. Blanditiis quisquam asperiores, vitae fugiat quia dolorum? A, voluptates libero.'/>
-                 <TestimonialsCard author='Sarah Johnson' text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae libero odio illo quasi eaque molestias obcaecati inventore atque ad est. Blanditiis quisquam asperiores, vitae fugiat quia dolorum? A, voluptates libero.'/>
-                 <TestimonialsCard author='Maria Rodriguez' text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae libero odio illo quasi eaque molestias obcaecati inventore atque ad est. Blanditiis quisquam asperiores, vitae fugiat quia dolorum? A, voluptates libero.'/>
+                <div className="">
+                    <SectionIntro header={<Button text="Testimonials" bg="bg-secondary" color="text-primary" width="w-52" fntSize="text-xl"/>}  name="Real Stories, Real Impact" />
+                </div>
+                <div className="flex flex-col gap-16 lg:flex-row">
+                    <TestimonialsCard author='Alex Chan' text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae libero odio illo quasi eaque molestias obcaecati inventore atque ad est. Blanditiis quisquam asperiores, vitae fugiat quia dolorum? A, voluptates libero.'/>
+                    <TestimonialsCard author='Sarah Johnson' text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae libero odio illo quasi eaque molestias obcaecati inventore atque ad est. Blanditiis quisquam asperiores, vitae fugiat quia dolorum? A, voluptates libero.'/>
+                    <TestimonialsCard author='Maria Rodriguez' text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae libero odio illo quasi eaque molestias obcaecati inventore atque ad est. Blanditiis quisquam asperiores, vitae fugiat quia dolorum? A, voluptates libero.'/>
+                </div>
             </div>
         </>
     )
